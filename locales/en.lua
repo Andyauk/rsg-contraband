59e382dd0c10282c2519c0afca6ecdf989a8f554
RexShack <rexshackuk@gmail.com>
2023-01-22 17:23:00 +0000

    tidy-up

--- locales/en.lua
+++ locales/en.lua
@@ -1,19 +1,19 @@
 local Translations = {
     error = {
-		you_do_not_have_enough_blood_money_to_do_that = 'You do not have enough blood money to do that!',
+        you_do_not_have_enough_blood_money_to_do_that = 'You do not have enough blood money to do that!',
         you_do_not_have_any_blood_money = 'You do not have any blood money!',
         you_do_not_have_enough_gold_bars = 'You do not have enough gold bars to do that!',
         you_do_not_have_any_gold_bars = 'You do not have any gold bars!',
     },
     success = {
-		you_sold_money_for = 'You sold %{amount} blood money for $ %{totalcash}',
+        you_sold_money_for = 'You sold %{amount} blood money for $ %{totalcash}',
         you_sold_gold_bars_for_totalcash = 'You sold %{amount} gold bars for $ %{totalcash}',
     },
     primary = {
         started_selling_contraband = 'started selling contraband',
     },
     menu = {
-		open = 'Open ',
+        open = 'Open ',
         outlaw_menu = '| Outlaw Menu |',
         blood_money_wash = 'Blood Money Wash',
         sell_gold_bars = 'Sell Gold Bars',
@@ -21,10 +21,10 @@ local Translations = {
         close_menu = 'Close Menu',
     },
     commands = {
-		var = 'text goes here',
+        var = 'text goes here',
     },
     progressbar = {
-		var = 'text goes here',
+        var = 'text goes here',
     },
     text = {
         wash_the_blood_off_your_money = 'wash the blood off your money',
--- locales/es.lua
+++ locales/es.lua
@@ -1,19 +1,19 @@
 local Translations = {
     error = {
-		you_do_not_have_enough_blood_money_to_do_that = '¡No tienes suficiente dinero de sangre para hacer eso!',
+        you_do_not_have_enough_blood_money_to_do_that = '¡No tienes suficiente dinero de sangre para hacer eso!',
         you_do_not_have_any_blood_money = '¡No tienes dinero de sangre!',
         you_do_not_have_enough_gold_bars = '¡No tienes suficientes lingotes de oro para hacer eso!',
         you_do_not_have_any_gold_bars = '¡No tienes lingotes de oro!',
     },
     success = {
-		you_sold_money_for = 'Vendistes %{amount} dinero de sangre para $ %{totalcash}',
+        you_sold_money_for = 'Vendistes %{amount} dinero de sangre para $ %{totalcash}',
         you_sold_gold_bars_for_totalcash = 'Vendistes %{amount} lingotes de oro para $ %{totalcash}',
     },
     primary = {
         started_selling_contraband = 'comenzó a vender contrabando',
     },
     menu = {
-		open = 'Abrir ',
+        open = 'Abrir ',
         outlaw_menu = '| Menú Outlaw |',
         blood_money_wash = 'Lavado de dinero con sangre',
         sell_gold_bars = 'Vender lingotes de oro',
@@ -21,10 +21,10 @@ local Translations = {
         close_menu = 'Cerrar menú',
     },
     commands = {
-		var = 'text goes here',
+        var = 'text goes here',
     },
     progressbar = {
-		var = 'text goes here',
+        var = 'text goes here',
     },
     text = {
         wash_the_blood_off_your_money = 'lava la sangre de tu dinero',
